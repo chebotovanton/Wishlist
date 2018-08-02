d0c5e262dde131c8064a0f65fd231a6fc8362860
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-08-02 19:44:24 +0300

    Initial Commit

---
+++ Wishlist/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wishlist
+//
+//  Created by Anton Chebotov on 02/08/2018.
+//  Copyright © 2018 Anton Chebotov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
