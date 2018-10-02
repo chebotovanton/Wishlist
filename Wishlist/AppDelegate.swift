c83a63b7c60a9b5611b16e6903f3655777984dfc
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-10-02 17:14:15 +0300

    Rates request logic

--- Wishlist/AppDelegate.swift
+++ Wishlist/AppDelegate.swift
@@ -6,9 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
-        UserDefaultsManager.increaseLaunchCount()
-
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.makeKeyAndVisible()
         let listVC = ListVC(nibName: "ListVC", bundle: nil)
@@ -16,36 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         navVC.isNavigationBarHidden = true
         window!.rootViewController = navVC
 
-        RateUsManager.requestReview()
-
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state.
-        // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message)
-        // or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers,
-        // and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
     func applicationDidBecomeActive(_ application: UIApplication) {
-        if let window = self.window, let controller = window.rootViewController {
-            PasteboardHandler.checkPasteboard(controller: controller)
+        checkPasteboard()
+    }
+
+    private func checkPasteboard() {
+        if let controller = self.window?.rootViewController, let appId = PasteboardHandler.getPasteboardId() {
+            proposeToAddAppId(appId: appId, controller: controller)
+        } else {
+            UserDefaultsManager.increaseLaunchCount()
+            RateUsManager.requestReview()
         }
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    private func proposeToAddAppId(appId: String, controller: UIViewController) {
+        //TODO: Got too weird
+        if let navVC = controller as? UINavigationController, let listVC = navVC.viewControllers.first as? ListVC, let presenter = listVC.presenter as? NewAppVCDelegate {
+            let newAppVC = NewAppVC(nibName: "NewAppVC", delegate: presenter, appId: appId)
+            controller.present(newAppVC, animated: true, completion: nil)
+        }
     }
-
 }
--- Wishlist/Utils/PasteboardHandler.swift
+++ Wishlist/Utils/PasteboardHandler.swift
@@ -2,13 +2,6 @@ import UIKit
 
 class PasteboardHandler {
 
-    static func checkPasteboard(controller: UIViewController) {
-        //tests!
-        if let appId = getPasteboardId() {
-            proposeToAddAppId(appId: appId, controller: controller)
-        }
-    }
-
     static func getPasteboardId() -> String? {
         for string in UIPasteboard.general.strings ?? [] {
             if let appId = AppIdExtractor.appIdFrom(urlString: string), !AppsKeeper.hasAppId(appId: appId) {
@@ -17,13 +10,5 @@ class PasteboardHandler {
         }
 
         return nil
-    }
-
-    private static func proposeToAddAppId(appId: String, controller: UIViewController) {
-        //TODO: Got weird
-        if let navVC = controller as? UINavigationController, let listVC = navVC.viewControllers.first as? ListVC, let presenter = listVC.presenter as? NewAppVCDelegate {
-            let newAppVC = NewAppVC(nibName: "NewAppVC", delegate: presenter, appId: appId)
-            controller.present(newAppVC, animated: true, completion: nil)
-        }
     }
 }
